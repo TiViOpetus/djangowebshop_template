d06ecba52efc56bfc39a75223f9c4681b5d20ec4
Markku Kynsijärvi <56072112+markkukynsijarvilektor@users.noreply.github.com>
2025-05-28 12:53:41 +0300

    added user registration

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:pk>/', views.product_details, name='product_details'),
     path('purchase_product/<int:pk>/', views.purchase_product, name='purchase_product'),
     path('purchase_succeeded/', views.purchase_succeeded, name='purchase_succeeded'),
+    path('register_user/', views.register, name='register_user'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product, Order
 from django.conf import settings
+from .forms import UserRegisterForm
 
 import requests
 import json
@@ -11,6 +12,18 @@ import hashlib
 
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'shop/register_user.html', { 'form': form})
+
+
 
 def product_list(request):
     products = Product.objects.all()
